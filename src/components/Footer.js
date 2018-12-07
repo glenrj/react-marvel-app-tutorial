37cee63f7b71f334d9d00c34b82e51811e443205
Glen McCann <glenmccann2@gmail.com>
2018-12-06 21:04:34 -0500

    Added header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer(props) {
+	return(
+		<footer>Data provided by Marvel. © 2014 Marvel</footer>
+		);
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+function Header(props) {
+	return (
+		<header>
+          <h1>{props.title}</h1>
+          <p>{props.message}</p>
+        </header>
+		);
+};
+
+export default Header;
