050242c48b97ed30fc13d53ab76a850c91c962d9
Glen McCann <glenmccann2@gmail.com>
2018-12-06 21:29:09 -0500

    Added filter bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import FilterBar from './FilterBar.js'
 
 function Header(props) {
 	return (
 		<header>
           <h1>{props.title}</h1>
           <p>{props.message}</p>
+          <FilterBar />
         </header>
 		);
 };
