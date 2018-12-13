6322ee7d6fd5a9775e5ecbc3cb31f74559d0a46b
Karley MacDonald <karleymacdonald@gmail.com>
2018-12-13 14:58:37 -0500

    Added FilterBar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.scss';
+import FilterBar from './FilterBar';
 
 const Header = (props) => {
 	return (
 		<header className="Header__Component">
 			<h1>{props.title}</h1>
 			<p>{props.message}</p>
+			<FilterBar />
 		</header>
 	);	
 };
