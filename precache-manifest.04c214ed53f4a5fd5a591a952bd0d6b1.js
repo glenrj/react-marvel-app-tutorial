e4c13cd8356b2cf034a4abd56657b28bdcfcfc47
Glen McCann <glenmccann2@gmail.com>
2018-12-21 12:47:22 -0500

    Updates

---
+++ precache-manifest.04c214ed53f4a5fd5a591a952bd0d6b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9036ce846f36f31aa54",
+    "url": "/react-marvel-app-tutorial/static/js/runtime~main.e9036ce8.js"
+  },
+  {
+    "revision": "78cede0e619fd2527117",
+    "url": "/react-marvel-app-tutorial/static/js/main.78cede0e.chunk.js"
+  },
+  {
+    "revision": "b5b88f090cae10725be1",
+    "url": "/react-marvel-app-tutorial/static/js/1.b5b88f09.chunk.js"
+  },
+  {
+    "revision": "78cede0e619fd2527117",
+    "url": "/react-marvel-app-tutorial/static/css/main.2c3f2053.chunk.css"
+  },
+  {
+    "revision": "15fba414ee8eb94f565f1d1cbf49a79f",
+    "url": "/react-marvel-app-tutorial/index.html"
+  }
+];
